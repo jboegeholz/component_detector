b2a9b726bca8046a125d8d78c83f58f3639de4db
jboegeholz <boegeholz.joern@gmail.com>
2026-06-26 12:44:01 +0200

    rework

--- component.py
+++ component.py
@@ -2,20 +2,12 @@ class Component:
 
     def __init__(self,
                  mpn="",
-                 manufacturer="",
-                 package="",
                  vds=None,
-                 current=None,
                  rds_on=None,
-                 logic_level=False):
-
+                 ):
         self.mpn = mpn
-        self.manufacturer = manufacturer
-        self.package = package
         self.vds = vds
-        self.current = current
         self.rds_on = rds_on
-        self.logic_level = logic_level
 
     def __str__(self):
         return self.mpn
