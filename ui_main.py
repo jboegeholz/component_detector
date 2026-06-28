1da95d43638081310feab8ad6fcb586302d08808
jboegeholz <boegeholz.joern@gmail.com>
2026-06-28 14:16:25 +0200

    rework

--- ui_main.py
+++ ui_main.py
@@ -175,10 +175,10 @@ class MainView(ui.View):
 
         self.result.text = (
             f"MPN {part.mpn}\n"
-            f"MPN {part.manufacturer}\n"
+            f"Hersteller {part.manufacturer}\n"
             f"VDS {part.vds} V\n"
             f"RDS(on) {part.rdson} Ω"
-            f"RDS(on) {part.continous_drain_current} A"
+            f"Dauerstrom {part.continous_drain_current} A"
         )
 
 
